16689ace627ed14182bade4e1b1f54c4429c4d52
nfpedraza <nifepegi@gmail.com>
2019-11-27 20:04:38 +0000

    arreglo

--- Clase_32.cpp
+++ Clase_32.cpp
@@ -2,68 +2,63 @@
 #include <cmath>
 #include <fstream>
 
-//Codigo extraido y modificado de : https://www.dreamincode.net/forums/topic/125770-simulating-diffusion-equation/
+//Para la resolucion del ejericico tome en cuenta la indicciones del libro, mis ejericios 29 y 25 para guiarme en el orden para declarar arreglos, matrices e imprimir datos.
 
 using namespace std;
 const double rho= 0.01;
-const double Tension=40;
-const int MaxT=1;
-const double dT =0.01;
-const int MaxL=1;
-const double dL= 0.01;
-void wave(int T_n,int L_n);
+const double MaxT=6.0, MaxL=1.0;
+const double dT =0.01, dL= 0.01; 
+int L_n = (MaxL/dL);
+const double c = 0.5;
+double cp=dL/dT;
+void wave(int L_n);
 
 int main ()
 {
-    wave(T_n,L_n);
+    wave(L_n);
     return 0;
 }
 
-void wave(int T_n, int L_n){
-    
-    int  T_n = MaxT/dT;
-	int  L_n = MaxL/dL;
-    double paso;
+void wave(int L_n){
+        
+    double paso, x, t;
 	
     ofstream fout("Clase_32.dat");
     
-    double **C = new double *[T_n+1];
-    for (int i=0;i<=T_n;i++)
-    {
-        C[i] =new double[L_n+1];
+    double *C = new double [L_n];
+    double *CP = new double [L_n];
+    double *CF = new double [L_n];
+    
+    for(int i=0;i<L_n+1;i++){
+    paso = i * dL;
+    C[i] = sin(M_PI * paso/MaxL); //condicion inicial
+        CP[i]=C[i];
+        CF[i]=C[i];
     }
     
-    
-    for(int i = 0; i <T_n; i++){
-        for(int j=1; j<L_n; j++){
+    for(int i = 1; i <L_n; i++){           
+            CP[i] = C[i] +(0.5*pow((c/cp),2)*(C[i+1]- 2*C[i] + C[i-1])); //aqui se define la ecuacion de difusion del libro  
+        }
+    t+=dT;
+    while (t<MaxT){
+        
+        for(int i =0;i<L_n+1;i++){
+            fout<<CP[i]<<"\t";
+        } fout<<"\n";
+        
+        for(int i=1;i<L_n;i++){
             
-            C[i+1][j] = C[i][j] +((D*dt/(pow(dx,2)))*(C[i][j+1] - 2*C[i][j] + C[i][j-1]))+(dt*S); //aqui se define la ecuacion de difusion del libro
-            C[0][j+1]=0.0;                             //boundary condition i)      
+        CF[i]= (2*CP[i] - C[i])+(C[i+1] - 2*C[i] + C[i-1]);
+        }
+        
+        for(int i=1;i<L_n;i++){
+             C[i]=CP[i];
+        CP[i]=CF[i];
+        }
+        t+=dT;
+        
+    }
    
-        C[x_n-1][j+1]=0.0;                        //boundary condition ii)   
-        }
-    }
-    
-    
-    double time =0;
-    for (int i = 0; i<=t_n; i++)
-    {
-        fout<<time<<"\t";
-        time+=dt;
-        
-        for (int j = 0; j <=x_n; j++)
-        {
-            fout<<C[i][j]<<"\t";
-        }
-        fout<<"\n";
-    }
-    
-    fout<<0<<"\t"; 
-    for(int j=0; j<=x_n;j++){
-        
-        fout<<x<<"\t";
-        x+= dx;
-    }
 
     fout.close();
 	                                    
--- Grafica.py
+++ Grafica.py
@@ -1,39 +1,29 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+
 
 data = np.loadtxt("Clase_32.dat")
+print(np.shape(data))
+x = np.linspace(0.0, 1.0, np.shape(data)[1])
+t = np.linspace(0.0, 6.0, np.shape(data)[0])
+X, T = np.meshgrid(x,t)
 
-dataT=data[:,0]
-data2=np.delete(data,0,0)
-data2=np.delete(data,-1,0)
+fig = plt.figure(figsize=(13,5))
+ax = fig.add_subplot(121, projection="3d")
+surf = ax.plot_surface(X, T, data, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+plt.xlabel("Posicion [metros[]")
+plt.ylabel("Tiempo [segundos]")
+ax.set_zlim3d(-1, 1)
+ax.view_init(20, 20)
+fig.colorbar(surf, shrink=0.5, aspect=5)
 
-x=np.linspace(0,100,30)
-y=np.linspace(0,12000,30)
-y2=np.linspace(0,1.2,30)
-y3=np.ones(30)*10**-6
-x2=np.e**-x
-
-plt.figure(figsize=(15,4))
-
-
-plt.subplot(1,3,1)
-plt.plot(data)
-plt.xlabel('$N_x$')
-plt.ylabel('# inte')
-
-plt.subplot(1,3,2)
-		 
-plt.plot(x2,y2, marker ='.')
-plt.xlabel('$N_x$')
-plt.ylabel('$|PSI_(0)-PSI_(0)analitica|$')
-
-plt.subplot(1,3,3)
-		 
-plt.plot(x,y3, marker ='.')
-plt.xlabel('$N_x$')
-plt.ylabel('$epsilon$')
-
-
-
-plt.savefig("difusion.png")
+plt.subplot(1,2,2)
+plt.plot(x, data[0,:], label="tiempo inicial")
+plt.plot(x, data[-1,:], label="tiempo final")
+plt.xlabel("Posicion [metros]")
+plt.ylabel("Desplazamiento [metros]")
+plt.legend()
+plt.savefig("plot.png", bbox_inches="tight")
